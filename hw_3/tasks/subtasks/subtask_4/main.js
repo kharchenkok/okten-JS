8ca7b059a70a1ab85e3fef1d6647f24197ac1038
kharchenkok <ekharchenko0108@gmail.com>
2025-02-09 02:25:19 +0200

    hw 3 - main tasks added

---
+++ hw_3/tasks/subtasks/subtask_4/main.js
@@ -0,0 +1,20 @@
+let textsArr=[
+    "OKTEN - школа програмування",
+    "2900 студентів",
+    "2500+ працевлаштованих студентів",
+    "з 2016 року навчаємо програмуванню",
+    "2500 працевлаштованих після курсів нашої іт-школи",
+    "Відгуки від роботодавців",
+    "підтверджена якість"
+]
+
+let counter = 0;
+document.write('<div class="container">');
+while (counter < 20){
+    counter += 1;
+    let randomText = textsArr[Math.floor(Math.random() * textsArr.length)];
+    document.write(`<h1 style="margin-bottom:20px; color: teal;">
+                    ${randomText}<span>(index - ${counter})</span>  
+                    </h1>`);
+}
+document.write('</div>');
